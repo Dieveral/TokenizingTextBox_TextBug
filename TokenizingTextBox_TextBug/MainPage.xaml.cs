f0647994dbc88a548217482608066544d8da41c8
Dieveral <dieveral@gmail.com>
2022-09-01 15:29:49 +0300

    Add project files.

---
+++ TokenizingTextBox_TextBug/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TokenizingTextBox_TextBug
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tokenizingBox.Text = string.Empty;
+        }
+    }
+}
